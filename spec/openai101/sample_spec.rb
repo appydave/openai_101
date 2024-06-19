dc515b6d004d4399f6312ef8355102bd8882d977
David Cruwys <david@ideasmen.com.au>
2024-06-19 10:48:35 +0700

    fix: tools enabled flag and node update on ci

--- spec/openai101/sample_spec.rb
+++ spec/openai101/sample_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe Openai101::Sample do
+RSpec.describe Openai101::Sample, :tools_enabled do
   it 'says hello' do
     expect(described_class.hello).to eq('Hello, World!')
   end
