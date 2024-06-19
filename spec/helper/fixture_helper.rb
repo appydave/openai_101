58f6c6a1fb0fcaf5db0587392f7a22224c1463e1
David Cruwys <david@ideasmen.com.au>
2024-06-20 05:22:10 +0700

    fix: models - list, retrieve

---
+++ spec/helper/fixture_helper.rb
@@ -0,0 +1,7 @@
+def fixture(file_name)
+  File.read(File.join(File.dirname(__FILE__), 'fixtures', file_name))
+end
+
+def fixture_path(file_name)
+  File.join(File.dirname(__FILE__), 'fixtures', file_name)
+end
--- spec/openai101/usecases/list_models_spec.rb
+++ spec/openai101/usecases/list_models_spec.rb
@@ -4,7 +4,15 @@ RSpec.describe 'List Models', :tools_enabled do
   let(:client) { OpenAI::Client.new }
 
   it 'list models' do
-    models = client.models.list['data'].reject { |model| model['owned_by'] == 'print-speak' }
+    models = client.models.list['data']
+      .reject { |model| model['owned_by'] == 'print-speak' }
+      .sort_by { |model| -model['created'] }
     tp models
+  end
+
+  it 'retrieve a model' do
+    model = client.models.retrieve(id: 'gpt-4o-2024-05-13')
+
+    tp [model]
   end
 end
